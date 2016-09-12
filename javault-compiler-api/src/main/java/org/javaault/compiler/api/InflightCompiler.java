18ad0740caba447b3a91277d5f82d62cfda80b33
dvekeman <dieter.vekeman@gmail.com>
2016-09-12 11:13:03 +0200

    javadoc

--- javault-compiler-api/src/main/java/org/javaault/compiler/api/InflightCompiler.java
+++ javault-compiler-api/src/main/java/org/javaault/compiler/api/InflightCompiler.java
@@ -2,9 +2,9 @@ package org.javaault.compiler.api;
 
 /**
  * Bridge interface between multiple java compiler implementation.
- * <br/>
- * INPUT: String (for now only String is supported but this could be extended later on)<br/>
- * OUTPUT: Class <br/> 
+ * <br>
+ * INPUT: String (for now only String is supported but this could be extended later on)<br>
+ * OUTPUT: Class <br> 
  */
 public interface InflightCompiler {
 	<T> Class<T> compileSource(String className, String source) throws CompilerException;
--- javault-core/src/main/java/org/javault/VaultRunner.java
+++ javault-core/src/main/java/org/javault/VaultRunner.java
@@ -5,23 +5,81 @@ import java.util.List;
 
 public interface VaultRunner {
 	/**
-	 * Main entry point to run code in a controller environment.
+	 * Run code in a controlled environment. The input is a directory containing one or more class files (or 
+	 * packages of class files) and a main method to be invoked.
 	 * 
 	 * Example: 
 	 * <pre>
 	 *   vaultRunner.runInVault0(Lists.newArrayList(evilCodeDirectory), "somepackage.SomeClass")
 	 * </pre>
+	 *
+	 * @see #runInVault0(List, String)
+	 * @see #runInVault0(String, String)
+	 * @see #runInVault0(Runnable)
+	 * @see #runInVault0(String)
 	 * 
 	 * @param paths Directories containing class files
 	 * @param runnableClass Main class to run
-	 *                         
+	 * @return The output result, system out and system error    
 	 * @throws VaultException
 	 */
 	VaultOutput runInVault0(List<URL> paths, String runnableClass) throws VaultException;
-	
+
+	/**
+	 * Run code in a controlled environment. The input is a class name and the source code for the class
+	 *
+	 * Example: 
+	 * <pre>
+	 *   vaultRunner.runInVault0("SomeClass", "System.out.println(\"Hello world\");")
+	 * </pre>
+	 *
+	 * @see #runInVault0(List, String)
+	 * @see #runInVault0(String, String)
+	 * @see #runInVault0(Runnable)
+	 * @see #runInVault0(String)
+	 * 
+	 * @param className The name of the class
+	 * @param source The source of the class
+	 * @return The output result, system out and system error    
+	 * @throws VaultException
+	 */
 	VaultOutput runInVault0(String className, String source) throws VaultException;
-	
+
+	/**
+	 * Run code in a controlled environment. The input is a Runnable class.
+	 *
+	 * Example: 
+	 * <pre>
+	 *   vaultRunner.runInVault0(myRunnable)
+	 * </pre>
+	 *
+	 * @see #runInVault0(List, String)
+	 * @see #runInVault0(String, String)
+	 * @see #runInVault0(Runnable)
+	 * @see #runInVault0(String)
+	 * 
+	 * @param runnable A <code>Runnable</code> class
+	 * @return The output result, system out and system error    
+	 * @throws VaultException
+	 */
 	VaultOutput runInVault0(Runnable runnable) throws VaultException;
-	
+
+	/**
+	 * Run code in a controlled environment. The input is a snippet (code without wrapping class)
+	 *
+	 * Example: 
+	 * <pre>
+	 *   vaultRunner.runInVault0("System.out.println(\"Hello world\");")
+	 * </pre>
+	 *
+	 * @see #runInVault0(List, String) 
+	 * @see #runInVault0(String, String)
+	 * @see #runInVault0(Runnable) 
+	 * @see #runInVault0(String) 
+	 *
+	 * @param snippet
+	 * @return
+	 * @throws VaultException
+	 */
 	VaultOutput runInVault0(String snippet) throws VaultException;
 }
